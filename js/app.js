2978a8b3d5f4de605c48aa60883002d6843f5bfd
OmicronDG <omicron_dg@mail.ru>
2018-04-21 21:44:05 +0300

    Add card list and fillCards method.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,41 @@
 /*
  * Create a list that holds all of your cards
  */
+const cards = document.querySelectorAll('.card');
 
+const diamond = "fa fa-diamond";
+const plane = "fa fa-paper-plane-o";
+const anchor = "fa fa-anchor";
+const bolt = "fa fa-bolt";
+const cube = "fa fa-cube";
+const leaf = "fa fa-leaf";
+const bicycle = "fa fa-bicycle";
+const bomb = "fa fa-bomb";
+
+const cardList = [diamond, diamond, plane, plane, anchor, anchor, bicycle, bicycle, bolt, bolt, cube, cube, leaf, leaf, bomb, bomb];
 
 /*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+* Display the cards on the page
+*   - shuffle the list of cards using the provided "shuffle" method below
+*   - loop through each card and create its HTML
+*   - add each card's HTML to the page
+*/
+
+//cards.forEach(element => element.setAttribute("class", "card show"));
+
+function fillCards(){
+    let shuffledCards = shuffle(cardList);
+
+    for(let e = 0; e < cards.length; e++){
+        let i = document.createElement('i');
+        i.setAttribute("class", `${shuffledCards[e]}`)
+        cards[e].appendChild(i);
+    }
+}
+
+function clearDesc(){
+    cards.forEach(element => element.firstChild.remove());
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
